f061f2ee66b0d5cb5dd7beeade1f7a3d603f077d
Jl3E <joey.elmblad@gmail.com>
2023-09-21 18:48:13 -0500

    initial set up

---
+++ hackathongo/src/main/java/com/springboard/hackathongo/HackathongoApplication.java
@@ -0,0 +1,13 @@
+package com.springboard.hackathongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathongoApplication.class, args);
+	}
+
+}
