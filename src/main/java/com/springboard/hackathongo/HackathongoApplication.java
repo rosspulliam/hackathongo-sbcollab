2b69a915fa6037f232b8620537c5cd8ebab10657
Jl3E <joey.elmblad@gmail.com>
2023-09-21 20:14:37 -0500

    move pom to start of directory again

---
+++ src/main/java/com/springboard/hackathongo/HackathongoApplication.java
@@ -0,0 +1,21 @@
+package com.springboard.hackathongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HackathongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathongoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {//test
+		return String.format("Hello %s!", name);
+	}
+}
