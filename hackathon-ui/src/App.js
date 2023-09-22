19eecb12059ed3c32cdc5367dcc693f28041580d
Walter Darby <walterdarbystcilien@gmail.com>
2023-09-22 14:16:54 -0400

    added login and signup page to routes

--- hackathon-ui/src/App.js
+++ hackathon-ui/src/App.js
@@ -1,16 +1,22 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Landing from './components/Landing/Landing';
+/** @format */
+
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing/Landing";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 
 function App() {
   return (
-    <div className="App">
-   <Router>
-<Routes>
-<Route path="/" element={<Landing/>} />
-</Routes>
-   </Router>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="/signup" element={<Signup/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
